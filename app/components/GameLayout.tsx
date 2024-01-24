898abaf61073ead4a9ab21e07eff774e6e8f2535
Hajin Lee <betterthanhajin@icloud.com>
2024-01-24 18:16:42 +0900

    feat: 진행중

--- app/components/GameLayout.tsx
+++ app/components/GameLayout.tsx
@@ -1,19 +1,27 @@
-import { MouseEventHandler } from "react";
-
+"use client";
+import { MouseEventHandler, useRef, createContext } from "react";
+export const context = createContext<HTMLDivElement | null>(null);
 const GameLayout = () => {
+  const backgroundRef = useRef<HTMLDivElement | null>(null);
   const handleMouseMove: MouseEventHandler<HTMLDivElement> = (e) => {
     console.log({ mousex: e.clientX, mousey: e.clientY });
   };
   return (
-    <div className="game-layout flex justify-between items-center w-full h-full">
-      <div className="game-layout__left">
-        <div className="w-3 h-14 bg-slate-500"></div>
+    <context.Provider value={backgroundRef.current}>
+      <div
+        className="game-layout flex justify-between items-center w-full h-full"
+        onMouseMove={handleMouseMove}
+        ref={backgroundRef}
+      >
+        <div className="game-layout__left">
+          <div className="w-3 h-14 bg-slate-500"></div>
+        </div>
+        <div className="game-layout__center w-0 h-full border-l-4 border-dotted border-l-slate-700"></div>
+        <div className="game-layout__right">
+          <div className="w-3 h-14 bg-slate-500"></div>
+        </div>
       </div>
-      <div className="game-layout__center w-0 h-full border-l-4 border-dotted border-l-slate-700"></div>
-      <div className="game-layout__right">
-        <div className="w-3 h-14 bg-slate-500"></div>
-      </div>
-    </div>
+    </context.Provider>
   );
 };
 export default GameLayout;
--- app/components/Pong.tsx
+++ app/components/Pong.tsx
@@ -1,8 +1,30 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef } from "react";
+import context from "./GameLayout";
 const Pong = () => {
+  const ballRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const ball = ballRef.current;
+    let direction = 1;
+    const moveBall = () => {
+      if (ball) {
+        const left = ball.style.left ? parseInt(ball.style.left) : 0;
+        if (left > window.innerWidth - 50) direction = -1;
+        if (left < 0) direction = 1;
+        ball.style.left = `${left + direction * 5}px`;
+      }
+    };
+    const intervalId = setInterval(moveBall, 20);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="pong-ball-container">
-      <div className="pong-ball w-8 h-8 absolute top-0 left-0 bg-teal-500 rounded-2xl"></div>
+      <div
+        ref={ballRef}
+        className="pong-ball w-8 h-8 absolute top-0 left-0 bg-teal-500 rounded-2xl"
+      ></div>
     </div>
   );
 };
